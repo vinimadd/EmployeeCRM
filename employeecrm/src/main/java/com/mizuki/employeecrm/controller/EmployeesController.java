e5284ba85a3e06151ee5e697e97c1f9dfff01cce
vinimadd <vinimadd@gmail.com>
2022-11-30 17:53:31 +0100

    Add website functionalities - update and delete employee Resolve #9

--- employeecrm/src/main/java/com/mizuki/employeecrm/controller/EmployeesController.java
+++ employeecrm/src/main/java/com/mizuki/employeecrm/controller/EmployeesController.java
@@ -7,9 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/employees")
@@ -35,5 +33,14 @@ public class EmployeesController {
     @ModelAttribute
     public Employee getEmployee() {
         return new Employee();
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam(name = "empId") Long id) {
+        log.info(id);
+        if(id != null) {
+            employeeService.deleteEmployee(id);
+        }
+        return "redirect:/employees";
     }
 }
